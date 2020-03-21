ee76ba95fb3c30adb10a0cd3ede4817b83c6f8be
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-03-21 21:17:33 +0000

    Added gradient in Computation

--- revgraph/core/computations/base/Computation.py
+++ revgraph/core/computations/base/Computation.py
@@ -1,7 +1,40 @@
 from abc import abstractmethod, ABC
+from collections import defaultdict
+from typing import Tuple, Optional
+
+import numpy as np
 
 
 class Computation(ABC):
+    def __init__(self,
+                 shape: Optional[Tuple[int, ...]] = None,
+                 requires_grad: bool = False):
+        self.gradient = None
+        self.shape = shape
+        self.requires_grad = requires_grad
+        self.references = defaultdict(lambda: 0)
+        self.current_count = 0
+        self.ctx = None
+
+    def new_session(self):
+        self.ctx = self.references.copy()
+
+    def accumulate(self, reference: 'Computation', gradient: np.array):
+        if self.ctx[reference] <= 0:
+            raise Exception('Repeated propagation of gradient')
+        else:
+            self.ctx[reference] -= 1
+        if self.shape is None:
+            self.shape = gradient.shape
+            self.gradient = gradient
+        else:
+            assert gradient.shape == self.gradient.shape
+            self.gradient += gradient
+
+    def register(self, reference: 'Computation'):
+        # Add a permanent computational node (independent of context)
+        self.references[reference] += 1
+
     @abstractmethod
     def forward(self):
         pass
