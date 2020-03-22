ea4abdf38d074eda0ff263311df453363e4cd8b7
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-03-22 23:24:22 +0000

    Added implementation for Function

---
+++ revgraph/core/computations/base/Function.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional, Tuple
+
+import numpy as np
+
+from .Computation import Computation
+
+
+class Function(Computation, ABC):
+    def __init__(self,
+                 args: List[Computation],
+                 shape: Tuple[Optional[int], ...],
+                 requires_grad: bool = False):
+        super(Function, self).__init__(shape, requires_grad)
+        self.args = args
+
+    def accumulate(self, reference: Computation, gradient: np.ndarray):
+        super(Function, self).accumulate(reference, gradient)
+        if self.context_completed():
+            self.backward()
+
+    @abstractmethod
+    def backward(self) -> None:
+        pass
