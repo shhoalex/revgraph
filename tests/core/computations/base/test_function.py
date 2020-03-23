b67e3bb6e466667fe78c001020c7d9657de2edef
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-03-23 16:31:39 +0000

    Added tests for Function

--- tests/core/computations/base/test_function.py
+++ tests/core/computations/base/test_function.py
@@ -35,3 +35,24 @@ class FunctionTestCase(unittest.TestCase):
         self.f.accumulate(self.parent, np.ones((2,3)))
         self.assertTrue(self.f.context_completed())
         self.assertEqual(np.ones((2,3)).all(), self.a.gradient.all())
+
+    def test_function_propagates_gradient_after_completion(self):
+        # Register twice
+        self.f.register(self.parent)
+        self.f.new_context()
+        self.a.new_context()
+
+        # Propagate the first gradient matrix
+        self.assertFalse(self.f.context_completed())
+        self.f.accumulate(self.parent, np.ones((2,3)))
+        self.assertFalse(self.f.context_completed())
+        self.assertFalse(self.a.context_completed())
+        self.assertIsNone(self.a.gradient)
+
+        # Propagate the second gradient matrix
+        x = np.arange(6).reshape(2,3)
+        self.f.accumulate(self.parent, x)
+        self.assertTrue(self.f.context_completed())
+        self.assertTrue(self.a.context_completed())
+        self.assertEqual((x+1).all(), self.f.gradient.all())
+        self.assertEqual((x+1).all(), self.a.gradient.all())
