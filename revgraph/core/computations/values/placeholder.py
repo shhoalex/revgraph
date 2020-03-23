f3f4729b23dc59ab4109060d9baf57fc2921a374
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-03-23 23:26:31 +0000

    Added implementation for Placeholder

--- revgraph/core/computations/values/placeholder.py
+++ revgraph/core/computations/values/placeholder.py
@@ -11,16 +11,21 @@ class Placeholder(Value):
                  default: Optional[Union[np.ndarray, list]] = None,
                  name: Optional[str] = None):
         super(Placeholder, self).__init__(shape=shape)
+        self.shape_constraint = shape
         self.default = default
         self.name = name
 
     def valid_shape(self, data: np.ndarray):
         if data.ndim != len(self.shape):
             return False
-        for i,j in zip(self.shape, data.shape):
+        for i,j in zip(self.shape_constraint, data.shape):
             if i is not None and i != j:
                 return False
         return True
+
+    def clear_value(self):
+        self.shape = self.shape_constraint
+        self.data = None
 
     def feed(self,
              data: Optional[Union[np.ndarray, list]] = None) -> Value:
@@ -30,4 +35,7 @@ class Placeholder(Value):
             data = np.array(self.default)
         if not self.valid_shape(data):
             raise ValueError(f'Expected shape: {self.shape}, got {data.shape} instead')
-        return Value(data=data, requires_grad=False)
+        super(Placeholder, self).__init__(data=data,
+                                          shape=data.shape,
+                                          requires_grad=False)
+
