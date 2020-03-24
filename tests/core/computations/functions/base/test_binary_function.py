5791dd9c97a2d8136d17954c0395cad035d01c8f
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-03-24 23:51:50 +0000

    Added tests for functions.base

---
+++ tests/core/computations/functions/base/test_binary_function.py
@@ -0,0 +1,54 @@
+import unittest
+
+import numpy as np
+
+from revgraph.core.computations.values.variable import Variable
+from revgraph.core.computations.functions.base.binary_function import BinaryFunction
+
+
+class BinaryFunctionImpl(BinaryFunction):
+    def apply(self,
+              a: np.ndarray,
+              b: np.ndarray) -> np.ndarray:
+        return a+b*2
+
+    def gradient_wrt_a(self,
+                       gradient: np.ndarray,
+                       a: np.ndarray,
+                       b: np.ndarray) -> np.ndarray:
+        return gradient
+
+    def gradient_wrt_b(self,
+                       gradient: np.ndarray,
+                       a: np.ndarray,
+                       b: np.ndarray) -> np.ndarray:
+        return gradient*2
+
+
+class BinaryFunctionTestCase(unittest.TestCase):
+    def setUp(self):
+        self.a = Variable([[1,2], [2,3]])
+        self.b = Variable([[4,5], [6,7]])
+        self.f = BinaryFunctionImpl(a=self.a,
+                                    b=self.b)
+        self.f.register(self.f)
+        self.f.new_context()
+
+    def test_correct_value_forwarded(self):
+        expected = np.array([[9,12], [14,17]])
+        actual = self.f.forward()
+        self.assertEqual(expected.all(), actual.all())
+
+    def test_correct_gradient_propagated_wrt_first_arg(self):
+        gradient = np.ones((2,2))
+        expected = np.ones((2,2))
+        self.f.accumulate(self.f, gradient)
+        actual = self.a.gradient
+        self.assertEqual(expected.all(), actual.all())
+
+    def test_correct_gradient_propagated_wrt_second_arg(self):
+        gradient = np.ones((2,2))
+        expected = np.full((2,2), 4)
+        self.f.accumulate(self.f, gradient)
+        actual = self.b.gradient
+        self.assertEqual(expected.all(), actual.all())
---
+++ tests/core/computations/functions/base/test_unary_function.py
@@ -0,0 +1,35 @@
+import unittest
+
+import numpy as np
+
+from revgraph.core.computations.values.variable import Variable
+from revgraph.core.computations.functions.base.unary_function import UnaryFunction
+
+
+class UnaryFunctionImpl(UnaryFunction):
+    def apply(self, a: np.ndarray) -> np.ndarray:
+        return a*2
+
+    def gradient_wrt_a(self,
+                       gradient: np.ndarray,
+                       a: np.ndarray) -> np.ndarray:
+        return gradient*2
+
+
+class UnaryFunctionTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.x = Variable([[1,2], [2,3]])
+        self.double = UnaryFunctionImpl(a=self.x)
+        self.double.register(self.double)
+        self.double.new_context()
+
+    def test_correct_value_forwarded(self):
+        expected = np.array([[2,4], [4,6]])
+        self.assertEqual(expected.all(), self.double.forward().all())
+
+    def test_correct_gradient_propagated(self):
+        gradient = np.ones((2,2))
+        expected = np.full((2,2), 4)
+        self.double.accumulate(self.double, gradient)
+        actual = self.x.gradient
+        self.assertEqual(expected.all(), actual.all())
