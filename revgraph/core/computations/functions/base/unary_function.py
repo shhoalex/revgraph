54d550793b3aacc35c8beeeea3a39a8b0050ef59
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-03-24 23:52:19 +0000

    Added implementation for functions.base

---
+++ revgraph/core/computations/functions/base/unary_function.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+from typing import Tuple
+
+import numpy as np
+
+from revgraph.core.computations.base.computation import Computation
+from revgraph.core.computations.base.function import Function
+
+
+class UnaryFunction(Function, ABC):
+    def __init__(self,
+                 a: Computation,
+                 shape: Tuple[int, ...] = None,
+                 requires_grad: bool = None):
+        requires_grad = a.requires_grad if requires_grad is None else requires_grad
+        super(UnaryFunction, self).__init__(args=[a],
+                                            shape=shape,
+                                            requires_grad=requires_grad)
+        self.a = a
+        self.a_res = None
+
+    def forward(self) -> np.ndarray:
+        self.a_res = self.a.forward()
+        return self.apply(a=self.a_res)
+
+    def backward(self) -> None:
+        if self.a.requires_grad:
+            ga = self.gradient_wrt_a(gradient=self.gradient,
+                                     a=self.a_res)
+            self.a.accumulate(self, ga)
+
+    @abstractmethod
+    def apply(self, a: np.ndarray) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def gradient_wrt_a(self,
+                       gradient: np.ndarray,
+                       a: np.ndarray) -> np.ndarray:
+        pass
