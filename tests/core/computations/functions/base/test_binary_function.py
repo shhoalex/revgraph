fa1e6b8523a943c933541fd6807939320cb0e24f
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-03-25 01:21:42 +0000

    Bug fixed: assert equal of np array should be (a==b).all() instead of (a.all() == b.all())

--- tests/core/computations/functions/base/test_binary_function.py
+++ tests/core/computations/functions/base/test_binary_function.py
@@ -48,7 +48,7 @@ class BinaryFunctionTestCase(unittest.TestCase):
 
     def test_correct_gradient_propagated_wrt_second_arg(self):
         gradient = np.ones((2,2))
-        expected = np.full((2,2), 4)
+        expected = np.full((2,2), 2)
         self.f.accumulate(self.f, gradient)
         actual = self.b.gradient
         self.assertTrue((expected == actual).all())
