26afd20fe8d30d2f9aa8cfec7d0c808bff539906
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-03-26 01:03:53 +0000

    Added implementation for Sub

---
+++ revgraph/core/computations/functions/sub.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from .base.binary_function import BinaryFunction
+
+
+class Sub(BinaryFunction):
+    def apply(self,
+              a: np.ndarray,
+              b: np.ndarray) -> np.ndarray:
+        return a-b
+
+    def gradient_wrt_a(self,
+                       gradient: np.ndarray,
+                       a: np.ndarray,
+                       b: np.ndarray) -> np.ndarray:
+        return gradient
+
+    def gradient_wrt_b(self,
+                       gradient: np.ndarray,
+                       a: np.ndarray,
+                       b: np.ndarray) -> np.ndarray:
+        return -gradient
