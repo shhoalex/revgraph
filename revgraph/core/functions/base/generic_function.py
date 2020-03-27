2714ed9158968acef901fdd9715d97d88670bdc4
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-03-27 20:09:58 +0000

    Added implementations for GenericFunction (Hacky)

---
+++ revgraph/core/functions/base/generic_function.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+from typing import Union, Any, Dict, Callable
+from inspect import getmembers
+
+import numpy as np
+
+
+from revgraph.core.base.computation import Computation
+from revgraph.core.base.function import Function
+
+
+GradientFunction = Callable[
+    ['GenericFunction', np.ndarray, np.ndarray, Any],
+    np.ndarray
+]
+
+
+def gradient_wrt_arg(key: Any) -> GradientFunction:
+    def decorator(function):
+        function.gradient_wrt = key
+        return function
+    return decorator
+
+
+class GenericFunction(Function, ABC):
+    def __init__(self,
+                 *args: Union[Computation, list, int, float],
+                 **kwargs: Any):
+        super(GenericFunction, self).__init__(args=args,
+                                              kwargs=kwargs)
+        self.results = None
+
+        # Hacky way to bind gradient function (for calculating
+        # derivative wrt arg[i]) with its respective argument
+
+        self.gradient_wrt_arg = {}
+        for _,m in getmembers(self):
+            if hasattr(m, 'gradient_wrt'):
+                self.gradient_wrt_arg[m.gradient_wrt] = m
+        self.gradient_wrt_arg_initialized = True
+
+    def forward(self) -> np.ndarray:
+        self.results = [arg.forward() for arg in self.args]
+        return self.apply(*self.results, **self.kwargs)
+
+    def backward(self) -> None:
+        for i,arg in enumerate(self.args):
+            if arg.requires_grad:
+                g_arg = self.gradient_wrt_arg[i](
+                    self.gradient,
+                    *self.results,
+                    **self.kwargs
+                )
+                arg.accumulate(self, g_arg)
+
+    @abstractmethod
+    def apply(self,
+              *args: np.ndarray,
+              **kwargs: Any) -> np.ndarray:
+        pass
