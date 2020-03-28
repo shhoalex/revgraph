49bcb43e7a2cab36a10b3d17ba2d1a2ad3009d5e
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-03-28 21:13:10 +0000

    Added tests for mathematical functions

---
+++ tests/core/functions/math/test_exp.py
@@ -0,0 +1,23 @@
+import unittest
+
+import numpy as np
+
+from revgraph.core.values.variable import Variable
+from revgraph.core.functions.math.exp import Exp
+
+
+class ExpTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.a = Variable([0,1,2,3])
+
+    def test_exp(self):
+        op = Exp(self.a)
+        op.register(op)
+        op.new_context()
+        expected = np.array([np.exp(i) for i in range(4)])
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.full((4,), 1) * [np.exp(i) for i in range(4)]
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
---
+++ tests/core/functions/math/test_log.py
@@ -0,0 +1,23 @@
+import unittest
+
+import numpy as np
+
+from revgraph.core.values.variable import Variable
+from revgraph.core.functions.math.log import Log
+
+
+class LogTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.a = Variable([1,2,3,4])
+
+    def test_exp(self):
+        op = Log(self.a)
+        op.register(op)
+        op.new_context()
+        expected = np.array([np.log(i) for i in range(1,5)])
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = 1/self.a.data
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
