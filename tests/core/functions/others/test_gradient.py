c2b168ed8b5b1acdae7ab38a6a5d5daff93ce560
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-03-30 01:05:54 +0100

    Added tests for Gradient

---
+++ tests/core/functions/others/test_gradient.py
@@ -0,0 +1,13 @@
+import unittest
+
+from revgraph.core.functions.others.gradient import Gradient
+from revgraph.core.values.variable import Variable
+
+
+class GradientTestCase(unittest.TestCase):
+    def test_gradient_is_propagated(self):
+        x = Variable(1)
+        y = Gradient(x)
+        y.new_context()
+        y.forward()
+        self.assertTrue(x.gradient == 1)
