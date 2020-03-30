b9d026ea6b7d8f0afde32cd67621d5b07782d1f7
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-03-30 18:31:14 +0100

    Added implementation of no_grad function

---
+++ revgraph/core/functions/base/no_grad_function.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+from revgraph.core.base.computation import Computation
+from revgraph.core.base.value import Value
+from .generic_function import GenericFunction
+
+
+class NoGradFunction(GenericFunction, ABC):
+    def __init__(self, *args, **kwargs):
+        super().__init__(**kwargs)
+        for arg in args:
+            if not isinstance(arg, Computation):
+                arg = Value(np.array(arg), requires_grad=False)
+            self.args.append(arg)
+
+    def backward(self, *args, **kwargs):
+        pass
+
+    def apply(self, *args, **kwargs):
+        return self.__class__.call(*args, **kwargs)
+
+    @staticmethod
+    @abstractmethod
+    def call(*args, **kwargs):
+        pass
