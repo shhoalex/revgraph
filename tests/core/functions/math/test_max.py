09340ad29a3656eccf7d9d9701873304643bb99b
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-04-01 21:53:01 +0100

    Added tests for max() and min()

---
+++ tests/core/functions/math/test_max.py
@@ -0,0 +1,107 @@
+import unittest
+
+import numpy as np
+
+from revgraph.core.values.variable import Variable
+from revgraph.core.functions.math.max import Max
+
+
+class MaxTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.a = Variable([[0,1,2],
+                           [-2,4,4],
+                           [1,9,5],
+                           [2,4,-2]])
+
+    def test_correct_return_value(self):
+        op = Max(self.a)
+        op.register(op)
+        op.new_context()
+        expected = 9
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.array([[0,0,0],
+                             [0,0,0],
+                             [0,1,0],
+                             [0,0,0]])
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
+
+    def test_correct_return_value_with_axis_equals_0(self):
+        op = Max(self.a, axis=0)
+        op.register(op)
+        op.new_context()
+        expected = [2,9,5]
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.array([[0,0,0],
+                             [0,0,0],
+                             [0,1,1],
+                             [1,0,0]])
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
+
+    def test_correct_return_value_with_axis_equals_1(self):
+        op = Max(self.a, axis=1)
+        op.register(op)
+        op.new_context()
+        expected = [2,4,9,4]
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.array([[0,0,1],
+                             [0,1,1],
+                             [0,1,0],
+                             [0,1,0]])
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
+
+    def test_correct_return_value_with_keepdims(self):
+        op = Max(self.a, keepdims=True)
+        op.register(op)
+        op.new_context()
+        expected = [[9]]
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.array([[0,0,0],
+                             [0,0,0],
+                             [0,1,0],
+                             [0,0,0]])
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
+
+    def test_correct_return_value_with_axis_equals_0_and_keepdims(self):
+        op = Max(self.a, axis=0, keepdims=True)
+        op.register(op)
+        op.new_context()
+        expected = [[2,9,5]]
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.array([[0,0,0],
+                             [0,0,0],
+                             [0,1,1],
+                             [1,0,0]])
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
+
+    def test_correct_return_value_with_axis_equals_1_and_keepdims(self):
+        op = Max(self.a, axis=1, keepdims=True)
+        op.register(op)
+        op.new_context()
+        expected = [[2],
+                    [4],
+                    [9],
+                    [4]]
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.array([[0,0,1],
+                             [0,1,1],
+                             [0,1,0],
+                             [0,1,0]])
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
---
+++ tests/core/functions/math/test_min.py
@@ -0,0 +1,107 @@
+import unittest
+
+import numpy as np
+
+from revgraph.core.values.variable import Variable
+from revgraph.core.functions.math.min import Min
+
+
+class MinTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.a = Variable([[0,1,2],
+                           [-2,4,4],
+                           [1,9,5],
+                           [2,4,-2]])
+
+    def test_correct_return_value(self):
+        op = Min(self.a)
+        op.register(op)
+        op.new_context()
+        expected = -2
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.array([[0,0,0],
+                             [1,0,0],
+                             [0,0,0],
+                             [0,0,0]])
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
+
+    def test_correct_return_value_with_axis_equals_0(self):
+        op = Min(self.a, axis=0)
+        op.register(op)
+        op.new_context()
+        expected = [-2,1,-2]
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.array([[0,1,0],
+                             [1,0,0],
+                             [0,0,0],
+                             [0,0,1]])
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
+
+    def test_correct_return_value_with_axis_equals_1(self):
+        op = Min(self.a, axis=1)
+        op.register(op)
+        op.new_context()
+        expected = [0,-2,1,-2]
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.array([[1,0,0],
+                             [1,0,0],
+                             [1,0,0],
+                             [0,0,1]])
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
+
+    def test_correct_return_value_with_keepdims(self):
+        op = Min(self.a, keepdims=True)
+        op.register(op)
+        op.new_context()
+        expected = [[-2]]
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.array([[0,0,0],
+                             [1,0,0],
+                             [0,0,0],
+                             [0,0,0]])
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
+
+    def test_correct_return_value_with_axis_equals_0_and_keepdims(self):
+        op = Min(self.a, axis=0, keepdims=True)
+        op.register(op)
+        op.new_context()
+        expected = [[-2,1,-2]]
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.array([[0,1,0],
+                             [1,0,0],
+                             [0,0,0],
+                             [0,0,1]])
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
+
+    def test_correct_return_value_with_axis_equals_1_and_keepdims(self):
+        op = Min(self.a, axis=1, keepdims=True)
+        op.register(op)
+        op.new_context()
+        expected = [[0],
+                    [-2],
+                    [1],
+                    [-2]]
+        actual = op.forward()
+        self.assertTrue((expected == actual).all())
+        op.accumulate(op, np.ones_like(expected))
+        expected = np.array([[1,0,0],
+                             [1,0,0],
+                             [1,0,0],
+                             [0,0,1]])
+        actual = self.a.gradient
+        self.assertTrue((expected == actual).all())
