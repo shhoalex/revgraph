fa62a61160e2ec5f492e7e29e5d0994aafd91edc
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-04-06 21:37:30 +0100

    Fixed bug in Conv1D

--- revgraph/core/functions/operations/conv1d.py
+++ revgraph/core/functions/operations/conv1d.py
@@ -7,12 +7,13 @@ def conv1d(a, b, stride=1):
     n_a, in_col, in_channel = a.shape
     n_b, in_channel_, out_channel = b.shape
     assert in_channel == in_channel_, f'in_channel must be equal'
-    r = (in_col - n_b + 1) // stride
+    r = ((in_col - n_b) // stride) + 1
     ans = np.zeros((n_a, r, out_channel))
 
-    for i in range(0, r, stride):
+    for i in range(0, r):
         for f in range(in_channel):
-            ans[:, i, :] += a[:, i:i+n_b, f] @ b[:, f, :]
+            i_ = i*stride
+            ans[:, i, :] += a[:, i_:i_+n_b, f] @ b[:, f, :]
     return ans
 
 
