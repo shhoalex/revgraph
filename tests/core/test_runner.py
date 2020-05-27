06136b50d70e3b020d877abf00b7fbba574f99e4
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-05-27 01:20:30 +0100

    Removed commented tests for old runner

--- tests/core/test_runner.py
+++ tests/core/test_runner.py
@@ -2,31 +2,8 @@ import unittest
 
 from revgraph.core.values.variable import Variable
 from revgraph.core.runner import *
-"""
 
-class RunnerTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.x = Variable([[0,0],
-                           [0,1]])
-        self.p = Placeholder(shape=(2,2), name='p')
-        self.expected = np.array([[0,0], [0,1]])
 
-    def test_correct_return_value(self):
-        runner = Runner(node=self.x)
-        actual = runner.run()
-        self.assertTrue((self.expected == actual).all())
-
-    def test_substitute_placeholder_by_reference(self):
-        runner = Runner(node=self.p)
-        actual = runner.run({self.p: self.x})
-        self.assertTrue((self.expected == actual).all())
-
-    def test_substitute_placeholder_by_name(self):
-        runner = Runner(node=self.p)
-        actual = runner.run({'p': self.x})
-        self.assertTrue((self.expected == actual).all())
-
-"""
 class RunTestCase(unittest.TestCase):
     def test_ignore_invalid_nodes(self):
         a = Variable(1)
