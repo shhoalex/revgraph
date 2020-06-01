08cc4e10155b9710921c096e4abbf6a1f0bde0b3
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-06-02 00:04:26 +0100

    Implemented decay for sgd

--- revgraph/core/functions/operations/gradient/optimizers/sgd.py
+++ revgraph/core/functions/operations/gradient/optimizers/sgd.py
@@ -2,9 +2,15 @@ from revgraph.core.functions.operations.gradient.base.optimizer import Optimizer
 
 
 class SGD(Optimizer):
-    def __init__(self, lr=0.001):
+    def __init__(self, lr=0.001, decay=0.0):
         super().__init__()
         self.lr = lr
+        self.decay = decay
+        self.iteration = 0
 
     def update(self, param, memory, global_memory):
-        param.data -= param.gradient * self.lr
+        lr = self.lr * (1.0 / (1.0 + self.decay * self.iteration)) \
+            if self.decay > 0 else self.lr
+        param.data -= param.gradient * lr
+        self.iteration += 1
+        print(lr)
