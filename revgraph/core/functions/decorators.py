b1fcc8f1e3362ae55f395a5093474cf630b32e4c
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-06-05 01:07:57 +0100

    Fixed no_grad function's qualified name

--- revgraph/core/functions/decorators.py
+++ revgraph/core/functions/decorators.py
@@ -2,7 +2,10 @@ from revgraph.core.functions.base.no_grad_function import NoGradFunction
 
 
 def no_grad(f):
-    class Function(NoGradFunction):
+    class NoGradDeriv(NoGradFunction):
+        __name__ = f.__name__
+        __qualname__ = f.__qualname__
+
         def call(*args, **kwargs):
             return f(*args, **kwargs)
-    return Function
+    return NoGradDeriv
