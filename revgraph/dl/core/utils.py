2f32bce3e0ea250f8ffde3d1a6d8c572012857e9
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-06-11 09:49:29 +0100

    Implemented global_registry

--- revgraph/dl/core/utils.py
+++ revgraph/dl/core/utils.py
@@ -1,6 +1,9 @@
 from .types import *
 
 
+global_registry: Dict[str, Any] = {}
+
+
 def get_graph(graph_instance: Metadata) -> rc.tensor:
     return graph_instance['graph']
 
@@ -15,6 +18,18 @@ def default_initializer(shape):
 
 def use_default_initializer(initializer: Initializer) -> Initializer:
     return use_default(initializer, default_initializer)
+
+
+def register(f: Callable) -> Callable:
+    global_registry[f.__qualname__] = f()
+    return f
+
+
+def register_as(key: str) -> Callable:
+    def decorator(f: Callable) -> Callable:
+        global_registry[key] = f()
+        return f
+    return decorator
 
 
 def validate(*predicates: Tuple[bool, str]) -> None:
