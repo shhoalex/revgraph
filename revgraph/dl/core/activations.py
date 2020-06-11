c66baf78d857dc66f5a9f143998bed38840b5667
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-06-11 09:49:43 +0100

    Implemented

---
+++ revgraph/dl/core/activations.py
@@ -0,0 +1,35 @@
+from .utils import *
+
+
+@register
+def relu() -> ActivationFunction:
+    def function(x: rc.tensor) -> rc.tensor:
+        return rc.max(x, 0)
+    return function
+
+
+@register
+def tanh() -> ActivationFunction:
+    return rc.tanh
+
+
+@register
+def sigmoid() -> ActivationFunction:
+    def function(x: rc.tensor) -> rc.tensor:
+        return 1 / (1 + rc.exp(-x))
+    return function
+
+
+@register
+def softmax(axis=1, keepdims=True) -> ActivationFunction:
+    def function(x: rc.tensor) -> rc.tensor:
+        e_x = rc.exp(x)
+        return e_x / e_x.sum(axis=axis, keepdims=keepdims)
+    return function
+
+
+@register
+def softplus() -> ActivationFunction:
+    def function(x: rc.tensor) -> rc.tensor:
+        return rc.log(rc.exp(x) + 1.0)
+    return function
