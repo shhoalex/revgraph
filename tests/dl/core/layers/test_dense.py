107cebe85f2b22c6af04e37836a6a6f347ef3bd3
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-06-11 02:35:15 +0100

    Added a basic test for testing the structure of the generated dense layer

--- tests/dl/core/layers/test_dense.py
+++ tests/dl/core/layers/test_dense.py
@@ -4,18 +4,23 @@ import revgraph.core as rc
 
 from revgraph.dl.core.layers.dense import dense
 
+from tests.utils import match_structure
+
 
 class DenseTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.placeholder = rc.placeholder(name='x', shape=(None, 10))
-        self.build_a = dense(units=10)
+        self.builder_basic = dense(units=10, use_bias=False)
 
     def tearDown(self) -> None:
         del self.placeholder
-        del self.build_a
+        del self.builder_basic
 
-    def test_generate_valid_metadata(self):
-        metadata = self.build_a({
+    def test_generate_valid_graph(self):
+        metadata = self.builder_basic({
             'units': 10,
-            'output': self.placeholder
+            'graph': self.placeholder
         })
+        expected = self.placeholder.dot(rc.variable([]))
+        actual = metadata['graph']
+        self.assertTrue(match_structure(expected, actual))
