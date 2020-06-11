1444e18c60066bb5dbb45a6a101529ebc5de4020
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-06-11 02:17:21 +0100

    Added util for testing the equivalence of 2 tensors structurally

---
+++ tests/utils.py
@@ -0,0 +1,50 @@
+import revgraph.core as rc
+
+
+def match_structure(a: rc.tensor, b: rc.tensor) -> bool:
+    """
+    See whether 2 tensors are equal structurally, not by value
+
+    Examples:
+    >>> x = rc.variable([1])
+
+    >>> a = rc.constant([0]) + rc.tanh(x) * rc.placeholder(name='x')
+    >>> b = rc.constant([1]) + rc.tanh(x) * rc.placeholder(name='x')
+    >>> match_structure(a, b)
+    True
+
+    >>> c = rc.constant(0) + rc.sinh(x) * rc.placeholder(name='x')
+    False
+
+    >>> d = rc.variable(0) + rc.tanh(x) * rc.placeholder(name='x')
+    >>> match_structure(a, d)
+    False
+
+    >>> e = rc.variable([0]) + rc.tanh(x) * rc.placeholder(name='y')
+    >>> match_structure(a, e)
+    False
+
+    >>> f = rc.constant([2]) + rc.tanh(rc.variable(2)) * rc.placeholder(name='x')
+    >>> match_structure(a, f)
+    True
+    """
+    if type(a) != type(b):
+        return False
+    if isinstance(a, rc.function_primitive):
+        a: rc.function_primitive
+        b: rc.function_primitive
+        if len(a.args) != len(b.args):
+            return False
+        for arg_a, arg_b in zip(a.args, b.args):
+            if not match_structure(arg_a, arg_b):
+                return False
+        return True
+    elif isinstance(a, rc.placeholder) and isinstance(b, rc.placeholder):
+        a: rc.placeholder
+        b: rc.placeholder
+        return a.name == b.name
+    elif isinstance(a, rc.variable) and isinstance(b, rc.variable):
+        a: rc.variable
+        b: rc.variable
+        return a.shape == b.shape
+    return True
