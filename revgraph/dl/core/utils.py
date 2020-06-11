22fea887c6f048acc5a6104fd45c9344dc31f274
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-06-11 01:40:26 +0100

    Added different utilities

--- revgraph/dl/core/utils.py
+++ revgraph/dl/core/utils.py
@@ -1,4 +1,20 @@
-from typing import List, Tuple
+from .types import *
+
+
+def get_graph(graph_instance: Metadata) -> rc.tensor:
+    return graph_instance['graph']
+
+
+def use_default(a: Any, b: Any) -> Any:
+    return a if a is not None else b
+
+
+def default_initializer(shape):
+    return np.random.randn(*shape)
+
+
+def use_default_initializer(initializer: Initializer) -> Initializer:
+    return use_default(initializer, default_initializer)
 
 
 def validate(*predicates: Tuple[bool, str]) -> None:
