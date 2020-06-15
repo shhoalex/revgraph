9f17df89e5778f7c81668eb24e378a503bd57d87
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-06-15 20:01:02 +0100

    Export flatten operation

--- revgraph/core/functions/operations/array/__init__.py
+++ revgraph/core/functions/operations/array/__init__.py
@@ -1,4 +1,5 @@
 from .conv1d import Conv1D as conv1d
 from .conv2d import Conv2D as conv2d
+from .flatten import Flatten as flatten
 from .pad import Pad as pad
 from .reshape import Reshape as reshape
