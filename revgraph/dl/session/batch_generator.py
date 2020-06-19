7780883992334cca875e71c20f52feb842a479cd
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-06-19 01:54:07 +0100

    Initial implementation of batch generator

---
+++ revgraph/dl/session/batch_generator.py
@@ -0,0 +1,21 @@
+from typing import *
+
+import numpy as np
+
+
+def batch_generator(x: np.array,
+                    y: np.array,
+                    batch_size: Optional[int] = None,
+                    shuffle: bool = False
+                    ) -> Generator[Tuple[np.array, np.array], None, None]:
+    if shuffle:
+        perm = np.random.permutation(len(x))
+        x = x[perm]
+        y = y[perm]
+
+    if batch_size is None:
+        yield (x, y)
+    else:
+        for i in range(0, len(x), batch_size):
+            batch = slice(i, i+batch_size)
+            yield (x[batch], y[batch])
