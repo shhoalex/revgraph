84500fc2a380e4234a80fcf286bc96b8986aac0d
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-06-24 17:36:40 +0800

    Implemented metrics

---
+++ revgraph/dl/core/metrics.py
@@ -0,0 +1,9 @@
+from .utils import *
+
+
+@register
+def categorical_accuracy() -> Metric:
+    def function(y_true: rc.tensor,
+                 y_pred: rc.tensor) -> rc.tensor:
+        return rc.sum(rc.argmax(y_true, axis=1) == rc.argmax(y_pred, axis=1)) / len(y_true)
+    return function
