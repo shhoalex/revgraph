07c1eae0b4cb33753f2b662877dee2c9f3777e5d
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-06-25 20:04:02 +0800

    Updated the definition of TensorFunction

--- revgraph/dl/core/types.py
+++ revgraph/dl/core/types.py
@@ -7,8 +7,8 @@ import revgraph.core as rc
 
 TensorShape = Union[int, Iterable[Union[None, int]]]
 Initializer = Union[str, Callable[[TensorShape], np.ndarray]]
-TensorFunction = ActivationFunction = Regularizer = Union[str, Callable[[rc.tensor], rc.tensor]]
+TensorFunction = Union[str, Callable[[rc.tensor, ...], rc.tensor]]
+Loss = Metric = ActivationFunction = Regularizer = TensorFunction
 Metadata = Dict[str, Any]
-Loss = Metric = Callable[[rc.tensor, rc.tensor], rc.tensor]
 GraphBuilder = Callable[[Metadata], Metadata]
 GraphBuilderNoParam = Callable[[], Metadata]
