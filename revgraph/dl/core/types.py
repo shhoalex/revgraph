ed2c12538097f0d5a4cd5f6d6908d08626fe068f
Shing Hin Ho <shinghinhoalex@gmail.com>
2020-06-26 11:27:42 +0800

    Bug fixed: Typing error

--- revgraph/dl/core/types.py
+++ revgraph/dl/core/types.py
@@ -7,7 +7,7 @@ import revgraph.core as rc
 
 TensorShape = Union[int, Iterable[Union[None, int]]]
 Initializer = Union[str, Callable[[TensorShape], np.ndarray]]
-TensorFunction = Union[str, Callable[[rc.tensor, ...], rc.tensor]]
+TensorFunction = Union[str, Callable[[rc.tensor], rc.tensor]]
 Loss = Metric = ActivationFunction = Regularizer = TensorFunction
 Metadata = Dict[str, Any]
 GraphBuilder = Callable[[Metadata], Metadata]
